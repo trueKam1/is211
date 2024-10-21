5f7ce92fa32d33a9ebd982e1e9915341b201ba55
trueKam1 <409-5@coopteh.local>
2024-10-21 14:05:47 +0700

    test-1

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -8,13 +8,13 @@ class RouterTest extends TestCase {
     public function test_router1() {
         $router = new Router();
         $html = $router->route( "http://localhost/orders" );
-        $pos= mb_strpos($html, "Создание заказа11");
+        $pos= mb_strpos($html, "Создание заказа");
         $this->assertNotEquals(false, $pos);
     }
     public function test_router2() {
         $router = new Router();
         $html = $router->route( "http://localhost" );
-        $pos= mb_strpos($html, "белый - фальшь");
+        $pos= mb_strpos($html, "Чёрный - стиль");
         $this->assertNotEquals(false, $pos);
     }
     public function test_router3() {
